c1113f14db715d9bca2d8a81d68d194896ac7ef4
Matthew Alfredo <matthewwalfredoo@gmail.com>
2023-09-05 11:42:12 +0700

    252 - Create REST API to Send Message

---
+++ src/main/java/com/learning/springbootrabbitmq/controller/MessageController.java
@@ -0,0 +1,30 @@
+package com.learning.springbootrabbitmq.controller;
+
+import com.learning.springbootrabbitmq.publisher.RabbitMQProducer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class MessageController {
+
+    private RabbitMQProducer producer;
+
+    public MessageController(RabbitMQProducer producer) {
+        this.producer = producer;
+    }
+
+    // http://localhost:8080/api/v1/publish?message=Hello%20World
+    @GetMapping("/publish")
+    public ResponseEntity<String> sendMessage(
+            @RequestParam(name = "message")
+            String message
+    ) {
+        producer.sendMessage(message);
+        return ResponseEntity.ok("Message sent to RabbitMQ...");
+    }
+
+}
