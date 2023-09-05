fa07fe8fdbc63cfe07f38bc2c6c5bd5d2051ccac
Matthew Alfredo <matthewwalfredoo@gmail.com>
2023-09-05 11:56:13 +0700

    254 - Configure RabbitMQ for JSON Message Communication

---
+++ src/main/java/com/learning/springbootrabbitmq/dto/User.java
@@ -0,0 +1,12 @@
+package com.learning.springbootrabbitmq.dto;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+
+}
